2d3d92ab34df07797abba4ad9871f998cf5a0c1c
wangxin <15234322925@163.com>
2019-11-04 16:59:43 +0800

    【目录】

--- src/main/java/com/wx/springBoot/demo01/web/controller/StudentController.java
+++ src/main/java/com/wx/springBoot/demo01/web/controller/StudentController.java
@@ -27,24 +27,12 @@ public class StudentController {
     @Resource
     private IStudentService studentService;
 
-    /*@GetMapping("/selectall")
+    @GetMapping("/selectall")
     public Wrapper<List<Student>> selectAll(){
         List<Student> students = studentService.studenetList();
         Wrapper<List<Student>> wrapper = null;
         if (null!=students){
             wrapper = WrapMapper.wrap(200,"查询成功",students);
-        }else {
-            wrapper = WrapMapper.wrap(200,"查无数据");
-        }
-        return wrapper;
-    }*/
-
-    @GetMapping("/selectTwoDataBase")
-    public Wrapper<Map<String, Object>> selectTwoDataBase(){
-        Map<String, Object> map = studentService.selectTwoDataBase();
-        Wrapper<Map<String, Object>> wrapper = null;
-        if (null!=map){
-            wrapper = WrapMapper.wrap(200,"查询成功",map);
         }else {
             wrapper = WrapMapper.wrap(200,"查无数据");
         }
